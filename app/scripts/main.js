be19e57ce7c124bbfca529fe93f7b48246ea99ef
JMaxwell <maxwell.joshua@gmail.com>
2014-01-22 17:30:39 -0500

    pretty cool looking

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,24 @@
 $(document).ready( function () {
   
   console.log('the document is ready and the script ran');
-  var windowHeight = $(window).height();
-  $('.hero').css('height', windowHeight-20);
+//I don't want this anymore....
+  // var windowHeight = $(window).height();
+  // $('.hero').css('height', windowHeight-20);
+
+  var numBoxes = $('.hero .container').children().length;
+  console.log(numBoxes);
+
+
+
+//next element sibling could do this?
+  // var x = 0;
+  // while (x < numBoxes - 1) {
+  //     console.log('in the numbering loop of div number', x = x+1)
+  //     console.log(x);
+  //     $('.hero .container:nth-child(x)').nth-child(1).firstChild.firstChild.text(x+1);
+  //     x = x + 1 ;
+  // }
+//I have to give this up now...hours happen quickly
 
 
 });
