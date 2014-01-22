78330b35cb856c5cfb2fb4d0f58969f97e8171f6
JMaxwell <maxwell.joshua@gmail.com>
2014-01-22 13:43:50 -0500

    finnaly wrangled some syntax to get the javascipt working

---
+++ app/scripts/another.js
@@ -0,0 +1,3 @@
+//This .js file is where I'll need to define 3
+// javascript classes and have them interact
+// with each other.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+//This main.js is where I'll need to bang
+//out 30 examples of jQuery usage
+
+$(document).ready( function () {
+  
+  console.log('the document is ready and the script ran');
+  var windowHeight = $(window).height();
+  $('.hero').css('height', windowHeight);
+
+
+});
