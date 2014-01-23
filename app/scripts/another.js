7ddf125964a1a8b19e510ba61e3a412110c9e593
JMaxwell <maxwell.joshua@gmail.com>
2014-01-23 04:55:59 -0500

    javascript objects assignment completed

--- app/scripts/another.js
+++ app/scripts/another.js
@@ -1,3 +1,86 @@
 //This .js file is where I'll need to define 3
 // javascript classes and have them interact
 // with each other.
+
+
+
+
+var altima = {
+  name: 'altima',
+  locked: false,
+  hitAtree: function(aPerson, aTree){
+    aPerson.haveKeys=false;
+    aTree.hitByCar = true;
+    aTree.height = aTree.height - 50;
+    aPerson.treesIveHit.push(aTree.type);
+    console.log("oh noes! " + aPerson.name + " just hit a " + aTree.type + " tree.")
+    aTree.speak();
+  },
+  speak: function() {
+    console.log('Hi, my name is', this.name, 'and I am');
+    if (!this.locked) {console.log('not locked')}
+      else {console.log('locked')};
+  }
+
+}
+
+var joshua = {
+  name: 'joshua',
+  haveKeys: true,
+  treesIveHit: [],
+  hitAtree: function(aCar, aTree){
+    this.haveKeys=false;
+    aTree.hitByCar = true;
+    aTree.height = aTree.height - 50;
+    this.treesIveHit.push(aTree.type);
+    console.log("oh noes! " + this.name + " just hit a " + aTree.type + " tree.")
+    aTree.speak();
+  },
+  lock: function(aCar) {
+    if (this.haveKeys) {
+      aCar.locked = true;
+      console.log(aCar.name + " was locked");
+    }
+    else
+      console.log("you don't have your keys!");
+  },
+  unlock: function(aCar) {
+    if (this.haveKeys) {
+      aCar.locked = false;
+      console.log(aCar.name + " was unlocked");
+    }
+    else
+      console.log("you don't have your keys!");
+  },
+  goFindKeys: function(){
+    this.haveKeys = true;
+    console.log('oh, here they are!');
+  },
+  speak: function() {
+    console.log('Hi, my name is', this.name);
+    if (this.haveKeys) {
+      console.log('and I have my keys!');
+    }
+    else
+    console.log('and I need to go find my keys!');
+  }
+}
+
+var pineTree = {
+  type: 'pine',
+  hitByCar: false,
+  height: 100,
+  speak: function(){
+    if(hitByCar){console.log("I've been hit!")};
+  }
+}
+
+var oakTree = {
+  type: 'oak',
+  hitByCar: false,
+  height: 80,
+  speak: function(){
+    console.log('Trees dont speak.');
+  }
+}
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,29 +8,54 @@ $(document).ready( function () {
   // var windowHeight = $(window).height();
   // $('.hero').css('height', windowHeight-20);
 
-//Here's example number 1
+  //Here's example number 1
   $('.color-changer').click ( function () {
     $(this).css('background', 'red');
   });
 
+
+  //Here's example number 2
   $('#example2 .button').click ( function () {
     var username = $('#example2 .username').val();
-    $('#example2 .result').text("Are you excited yet, " + username + "? Because I am!");
+    $('#example2 .result').text("Are you excited yet, " + username + "? Because I am! Click 'submit' again to make this text disappear");
+    $('#example2 .result').toggleClass('reveal');
   });
 
     //there must be a better way than copy pasting like this
   $('#example2 .username').blur ( function () {
     var username = $('#example2 .username').val();
     $('#example2 .result').text("Are you excited yet, " + username + "? Because I am!");
+    $('#example2 .result').toggleClass('reveal');
+  });
+
+  //Here's example number 3
+  $('#example3 .ball').click( function () {
+    $(this).toggleClass('dropped');
+  });
+
+  //Here's example number 4
+  $('#example4 .gun').click( function (){
+    console.log('fire zee weapon!')
+    $('#example4 .bullet').addClass('fired');
+    $('#example4 .example').prepend('<div class="bullet">div</div>');
+    $('#example4 .gun').toggleClass('fired');
+  });
+
+  //Here's example number 5
+  $('#example5 .button').click( function (){
+    $('#example5 .example').append('<div class="ball"></div>');
+    var circleSize = $('#example5 input').val();
+    console.log(circleSize);
+    $('#example5 .ball').last().css('margin', 5);
+    $('#example5 .ball').last().animate({width: circleSize+'px', height: circleSize+'px'}, 100 );
   });
 
 
-//example number 2
 
-
+//I thought i could get jquery to number the boxes for me but i'm not
+//good enough at it yet
   // var numBoxes = $('.hero .container').children().length;
   // console.log(numBoxes);
-//next element sibling could do this?
   // var x = 0;
   // while (x < numBoxes - 1) {
   //     console.log('in the numbering loop of div number', x = x+1)
@@ -59,7 +84,7 @@ $(".link-div > a").click(function(event){
 
     //get the top offset of the target anchor
     var target_offset = $("#"+trgt).offset();
-    var target_top = target_offset.top;
+    var target_top = target_offset.top - 50;
 
     //goto that anchor by setting the body scroll top to anchor top
     $('html, body').animate({scrollTop: target_top }, 600);
